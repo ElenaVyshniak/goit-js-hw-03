9d2377f947fe23397c45ff92a3ab114f311a0a1b
ElenaVyshniak <138595658+ElenaVyshniak@users.noreply.github.com>
2024-01-31 09:53:20 +0200

    tasks 1

---
+++ js/tasks-1.js
@@ -0,0 +1,22 @@
+// Задача 1. Генератор slug
+// Slug — це завжди рядок у нижньому регістрі, слова якого розділені тире.
+// З цим розібралися? А тепер давай нарешті виконувати задачу!
+// Напиши функцію slugify(title), яка приймає заголовок статті, параметр title і повертає slug, створений із цього рядка.
+// Значенням параметра title будуть рядки, слова яких розділені лише пробілами.
+// Усі символи slug повинні бути в нижньому регістрі.
+// Усі слова slug повинні бути розділені тире.
+
+function slugify(title) {
+  // Перетворення всіх символів у заголовку в нижній регістр
+  title = title.toLowerCase();
+  // Розділення рядка на слова за допомогою методу split() і пробілу як розділювача
+  const words = title.split(" ");
+  // Об'єднання слів за допомогою тире
+  const slug = words.join("-");
+  return slug;
+}
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
